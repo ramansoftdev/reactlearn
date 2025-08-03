6e406f3b5a52d9ea1cf15626fe38de5c82c359ca
ramansoftdev <ramansoftdev@gmail.com>
2025-08-03 22:45:10 +0530

    complete game fetched harry potter characters from api

--- hangmangame/src/App.jsx
+++ hangmangame/src/App.jsx
@@ -1,63 +1,80 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import LifeLine from "./components/LifeLine";
 import { clsx } from "clsx";
+import Confetti from "react-confetti";
 
 function App() {
   //static values
-  const alphabets = "abcdefghijklmnopqrstuvwxyz";
+  const alphabets = "abcdefghijklmnopqrstuvwxyz".toUpperCase();
   const totalLives = 10;
 
   // state values
-  const [currentWord, setCurrentWord] = useState("ganesha");
+  const [words, setWords] = useState([]);
+  const [currentWord, setCurrentWord] = useState("");
+  const [currentWordHint, setCurrentWordHint] = useState("");
+
   const [guessedLetters, setGuessedLetters] = useState(new Set());
+  const [prevGuessLetter, setPrevGuessLetter] = useState("");
 
   //derived values
-  let wrongLetterCount = 0;
-  let correctLettersCount = 0;
-  let gameWon = false;
-  let gameLost = false;
-  let gameOver = false;
+  let wrongLetterCount = currentWord
+    ? [...guessedLetters].filter((letter) => !currentWord.includes(letter))
+        .length
+    : 0;
+  let gameWon =
+    currentWord &&
+    currentWord.split("").every((letter) => guessedLetters.has(letter));
+  let gameLost = currentWord && wrongLetterCount >= totalLives;
+  let gameOver = (currentWord && gameWon) || gameLost;
+  let prevGuessCorrect =
+    currentWord &&
+    prevGuessLetter !== "" &&
+    currentWord.includes(prevGuessLetter);
+  // console.log("prvious letter is " + prevGuessLetter + " and previous correct is " +  prevGuessCorrect)
 
-  for (const letter of guessedLetters) {
-    if (!currentWord.includes(letter)) {
-      wrongLetterCount += 1;
-    }
-  }
-
-  for (const letter of currentWord) {
-    if (guessedLetters.has(letter)) {
-      correctLettersCount += 1;
-    }
-  }
-
-  // console.log(`u hav used ${usedLives} lives out of ${totalLives}`)
-
-  if (wrongLetterCount >= totalLives) {
-    gameLost = true;
-  } else if (correctLettersCount === currentWord.length) {
-    gameWon = true;
-  }
-
-  gameOver = gameWon || gameLost;
-
-  if (gameLost) {
-    console.log("you have lost the game");
-  } else if (gameWon) {
-    console.log("you have won the game");
-  }
+  // functions
 
   const handleKeyClick = (letter) => {
+    if (!currentWord) {
+      return;
+    }
+    const newPrevGuess = letter;
+    setPrevGuessLetter(newPrevGuess);
     setGuessedLetters((prevLetters) => {
       const newLetters = new Set(prevLetters);
-      newLetters.add(letter);
+      newLetters.add(letter.toUpperCase());
       return newLetters;
     });
   };
 
+  const renderGameStatus = () => {
+    if (gameWon) {
+      return (
+        <>
+          <h2>You Won!!, play Again!</h2>
+          <p>"Well Done!!</p>
+        </>
+      );
+    } else {
+      return (
+        <>
+          <h2>You Lost!</h2>
+          <p>Try Again!</p>
+        </>
+      );
+    }
+  };
+
   const guessedWordList = currentWord.split("").map((letter, index) => {
+    const shouldRevealLetter = guessedLetters.has(letter) || gameLost;
+    const className = clsx(
+      gameLost && !guessedLetters.has(letter) && "missed-letter"
+    );
+
+    // console.log(" currentword is " + currentWord + "guessed letter si " + [...guessedLetters] + "letter is "+ letter + " and should reveal is " + shouldRevealLetter)
     return (
-      <span key={index} className="guessed-char">
-        {guessedLetters.has(letter) && letter.toUpperCase()}
+      <span key={index} className={className}>
+        {shouldRevealLetter && letter.toUpperCase()}
       </span>
     );
   });
@@ -79,24 +96,81 @@ function App() {
         className={className}
         key={letter}
         onClick={() => handleKeyClick(letter)}
+        disabled={gameOver || guessedLetters.has(letter)}
+        aria-disabled={gameOver || guessedLetters.has(letter)}
+        aria-label={`Letter ${letter}`}
       >
         {letter.toUpperCase()}
       </button>
     );
   });
 
+  const handleNewGameClick = () => {
+    if (!gameOver) {
+      return;
+    }
+    getRandomWord();
+    setGuessedLetters(new Set());
+    setPrevGuessLetter("");
+  };
+
+  const fetchWords = () => {
+    fetch("https://hp-api.onrender.com/api/characters")
+      .then((res) => res.json())
+      .then((characters) => {
+        // console.log("First character:", characters[0].actor);
+        // console.log("all char are" + characters.map((character)=>character.name))
+        // console.log("First character:", characters[0].alternate_names[0]);
+        setWords(
+          characters
+            .filter((character) => character.alternate_names.length>0)
+            .map((character) => [
+              character.name.toUpperCase().replaceAll(" ", ""),
+              character.alternate_names[0],
+            ])
+        );
+      })
+      .catch((err) => console.log("error api call " + err));
+  };
+
+  const getRandomWord = () => {
+    const index = Math.floor(Math.random() * words.length);
+    setCurrentWord(words[index][0]);
+    setCurrentWordHint(words[index][1]);
+    console.log(
+      // "current word is " + words[index][0] + " and hint is " + words[index][1]
+    );
+  };
+
+  useEffect(fetchWords, []);
+
   return (
     <>
       <main>
+        {gameWon && <Confetti numberOfPieces={1000} />}
         <header>
-          <h1>Hangman Game:</h1>
-          <p> You have {totalLives} lives to guess the correct word!</p>
+          <h1>Hangman Game</h1>
+          <p>
+            {" "}
+            You have {totalLives} lives to guess this HARRYPOTTER character!
+          </p>
         </header>
 
+        {!gameOver && prevGuessLetter && (
+          <section className="guess-message">
+            {prevGuessLetter.toUpperCase()} is{" "}
+            {prevGuessCorrect ? "correct!!" : "incorrect!"}
+          </section>
+        )}
+
         {gameOver && (
-          <section className="game-status" style={{backgroundColor :  gameWon ? "#10A95B" : "red"}}>
-            <h2>{gameWon ? "You Won!!" : "You Lost!"}</h2>
-            <p>{gameWon ? "Well Done!!" : "Try Again!"}</p>
+          <section
+            className="game-status"
+            style={{ backgroundColor: gameWon ? "#10A95B" : "#BA2A2A" }}
+            aria-live="polite"
+            role="status"
+          >
+            {renderGameStatus()}
           </section>
         )}
 
@@ -106,9 +180,25 @@ function App() {
 
         <section className="guessed-word">{guessedWordList}</section>
 
+        {currentWordHint && (
+          <section>
+            <p className="hint">Hint: {currentWordHint}</p>
+          </section>
+        )}
+
         <section className="keyboard">{alphabetsList}</section>
 
-        <button className="new-game-button">New Game</button>
+        {gameOver && (
+          <button onClick={handleNewGameClick} className="new-game-button">
+            New Game
+          </button>
+        )}
+
+        {!currentWord && (
+          <button onClick={getRandomWord} className="new-word-button">
+            Get New Word
+          </button>
+        )}
       </main>
     </>
   );
