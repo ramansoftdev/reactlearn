37fd1617ba07b48e7373068b7280a5671e171ea0
ramansoftdev <ramansoftdev@gmail.com>
2025-08-23 23:47:15 +0530

    removing randommeme hangmangame tenziegame and adding them as submodules

--- useeffectproject/memegenerator/src/components/Header.jsx
+++
@@ -1,8 +0,0 @@
-export default function Header() {
-  return (
-    <header className="header">
-      <img src="/src/assets/troll-face.png" />
-      <h1>Meme Generator</h1>
-    </header>
-  );
-}
